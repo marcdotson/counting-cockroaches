5aa5b0f96878c59b95d5547725cdebd8cecc2fdb
adrielc <adrielcasellas@gmail.com>
2019-03-01 14:50:56 -0700

    Add script to pull tweets with predictions from best current model

--- Code/rds-data-processing/classifier-testing.R
+++ Code/rds-data-processing/classifier-testing.R
@@ -1,12 +1,13 @@
 library(fastrtext)
 library(Matrix)
 library(xgboost)
-source("R/predict_func.R")
-source("R/TweetComplaintPredictor.r")
+source("Code/complaint-prediction/R/predict_func.R")
+source("Code/complaint-prediction/R/TweetComplaintPredictor.r")
 
-load("complaint-prediction/models/fullClassifierOne/prcomp4.dat")
-fasttextModel <- load_model("complaint-prediction/models/fullClassifierOne/model50-tweet_sample_2M_noRT.dat.bin")
-load("complaint-prediction/models/fullClassifierOne/xgb.modelPC4.dat")
+load("Code/complaint-prediction/models/fullClassifierOne/prcomp4.dat")
+fasttextModel <- load_model("Code/complaint-prediction/models/fullClassifierOne/model50-tweet_sample_2M_noRT.dat.bin")
+load("Code/complaint-prediction/models/fullClassifierOne/xgb.modelPC4.dat")
 TweetComplaintPredictor <- newTweetComplaintPredictor(fasttextModel, train.pr, bst, PC4)
 
-predict(TweetComplaintPredictor, "")
+
+predict(TweetComplaintPredictor, "@delta I hate you sooooo freaking much you made me late to my meeting in Denver")
