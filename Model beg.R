5d6dc20c03d85832c5f1ab6511490e02e9111d66
Adam Hettinger <adam.kyle.hettinger@gmail.com>
2019-07-16 16:36:07 -0600

    adding in rethinking notation and quap

--- Model beg.R
+++ Model beg.R
@@ -56,3 +56,23 @@ plot(fit)
 
 
 #Got it!
+
+#inserting chapter 5 p146 notation and quap formula
+
+#Ki ∼ Normal(µi, σ)
+#µi = α + βNNi + βMMi
+#α ∼ Normal(0, 0.2)
+#βn ∼ Normal(0, 0.5)
+#βm ∼ Normal(0, 0.5)
+#σ ∼ Exponential(1)
+
+m5.7 <- quap(
+  alist(
+    K ~ dnorm( mu , sigma ) ,
+    mu <- a + bN*N + bM*M ,
+    a ~ dnorm( 0 , 0.2 ) ,
+    bN ~ dnorm( 0 , 0.5 ) ,
+    bM ~ dnorm( 0 , 0.5 ) ,
+    sigma ~ dexp( 1 )
+  ) , data=dcc )
+precis(m5.7)
