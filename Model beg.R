103ee1b3478c69aff1c6a298fc2ffc2e9b6bb6bd
Adam Hettinger <adam.kyle.hettinger@gmail.com>
2019-07-17 14:57:12 -0600

    I am trying to use other functions to plot our model, but no luck.

--- Model beg.R
+++ Model beg.R
@@ -34,23 +34,23 @@ library(rethinking)
 
 #simulated data for multiple regression 
 
-mydata <- sample(1:7, size=100, replace=TRUE )
+mydata <- sample(1:7, size=1000, replace=TRUE )
 #regress <- lm( mydata ~ x1 + x2)
 #plot(regress)
 
 #creating variables 
 #length of tweet 
 # I set between 10 and 280 because 280 is the max on twitter and 10 seemed like a small enough number 
-length <- sample(10:280, size = 100 , replace = TRUE )
+length <- sample(10:280, size = 1000 , replace = TRUE )
 
 #Have they tweeted about this in the past 
-past <- sample(1:2 , size = 100 , replace = TRUE )
+past <- sample(1:2 , size = 1000 , replace = TRUE )
 
 #How many times has this re-tweeted 
-re_tweet <- runif(100, min=0, max=5)
+re_tweet <- runif(1000, min=0, max=5)
 
 #How many followers they have, I looked it up and the average twitter user has about 700 followers
-followers <- rnorm(n = 100 , mean = 700 , sd = 200 )
+followers <- rnorm(n = 1000 , mean = 700 , sd = 200 )
 
 # I think we could use rnorm for followers becasue we know the mean (I google it) 
 #but for re_tweet it might be better to use a uniform distribution because we don't know anything about how it is distributed 
@@ -58,6 +58,7 @@ followers <- rnorm(n = 100 , mean = 700 , sd = 200 )
 Adam
 # Makes sense to me. re_tweet could very well be exponential or normal, but I couldn't give a great reason for either
 #I also thought about adding number of negative tweets, or is that the vairable 'past'?
+#is there another way to plot this that makes sense?
 
 #combine into data frame 
 d <- data.frame(length, past, re_tweet, followers)
@@ -75,7 +76,6 @@ twitter <- quap(
     ) , data=d) 
 precis(twitter)
 plot(twitter)
-
 
 #inserting chapter 5 p146 notation and quap formula
 
