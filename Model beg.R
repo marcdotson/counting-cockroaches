ae301440b058da053843186d4c4a696be59c994b
McKenna Weech <mckennaweech@outlook.com>
2019-07-25 16:02:18 -0600

    Making a model

--- Model beg.R
+++ Model beg.R
@@ -5,52 +5,52 @@ library(rethinking)
 library(devtools)
 library(usethis)
 
-# Simulated data for multiple regression.
 
-# mydata <- sample(1:7, size=1000, replace=TRUE )
-#regress <- lm( mydata ~ x1 + x2)
-#plot(regress)
-
-#creating variables
 #length of tweet
-# I set between 10 and 280 because 280 is the max on twitter and 10 seemed like a small enough number 
 length_of_tweet <- sample(10:280, size = 1000 , replace = TRUE )
 
-#Have they tweeted about this in the past 
 
+#Have they tweeted about this in the past 
 past <- sample(0:1 , size = 1000 , replace = TRUE )
 
 
 #How many times has this re-tweeted 
 re_tweet <- runif(1000, min=0, max=5)
 
-# follower count and re-tweet count may be too correlated to run in the same model 
  
-#How many followers they have, I looked it up and the average twitter user has about 700 followers
+#How many followers they have
 followers <- sample(0:1000, size=1000, replace=TRUE)
 
-# I think we could use rnorm for followers becasue we know the mean (I google it) 
-#but for re_tweet it might be better to use a uniform distribution because we don't know anything about how it is distributed 
-# I've also aribitraily made our sample size 100 but we can change that if we think something else would be more appropriate 
+#scaling data 
+length_of_tweet_scaled <- scale(length_of_tweet)
 
-# Makes sense to me. re_tweet could very well be exponential or normal, but I couldn't give a great reason for either
-#I also thought about adding number of negative tweets, or is that the vairable 'past'? M- The variable past was suppose to be if they have tweeted in the past of not
+past_scaled <- scale(past)
 
-#is there another way to plot this that makes sense?
+re_tweet_scaled <- scale(re_tweet)
+
+followers_scaled <- scale(followers)
+
+View(d_scaled)
 
 # Specify parameter values.
-beta0 <- 1
-beta1 <- 3
-beta2 <- 2
-beta3 <- 4
-beta4 <- 5
-error <- rnorm(length(past), 0, 1)
+b0 <- 1
+bl <- 1
+bp <- 1
+br <- 1
+bf <- 10
+error <- rnorm(1000, 0, 1) 
 
 # Generate y.
-mydata <- beta0 + beta1 * length + beta2 * past + beta3 * re_tweet + beta4 * followers + error
+mydata <- b0 + bl * length_of_tweet + bp * past + br * re_tweet + bf * followers + error
+
+#Generate y_scaled 
+mydata_scaled <- b0 + bl *length_of_tweet_scaled + bp * past_scaled + br * re_tweet_scaled + bf * followers_scaled + error
 
 # Combine into data frame.
-d <- data.frame(mydata, length, past, re_tweet, followers)
+d <- data.frame(mydata, length_of_tweet, past, re_tweet, followers)
+
+#new scaled data frame
+d_scaled <- data.frame(length_of_tweet_scaled,past_scaled,re_tweet_scaled, followers_scaled)
 
 # Estimation --------------------------------------------------------------
 # Run quap.
@@ -67,46 +67,33 @@ twitter <- quap(
   data = d
 )
 
+#quap scaled 
+twitter_scaled <- quap(
+  alist( 
+    mydata_scaled ~ dnorm(mu, sigma) , 
+    mu <- bl*length_of_tweet_scaled + bp*past_scaled + br*re_tweet_scaled + bf*followers_scaled , 
+    bl ~ dnorm(0, .25), 
+    bp ~ dnorm(0, .25), 
+    br ~ dnorm(0, .25), 
+    bf ~ dnorm(0, .25), 
+    sigma ~ dexp(1) 
+  ), 
+  data = d_scaled
+)
+
 # Evaluating model fit.
-precis(twitter)
-plot(twitter)
+precis(twitter_scaled)
+plot(twitter_scaled)
 
 #trying to run the model without "weights"
-plot(mydata)
+plot(mydata_scaled)
 
-twitter <- quap(
-  alist( 
-    mydata ~ dnorm(mu, sigma) , 
-    mu <- length + past + re_tweet + followers , 
-    sigma ~ dexp(1) 
-  ) , data=d) 
-precis(twitter)
-plot(twitter)
+impact <- lm(mydata_scaled~followers_scaled, data=d_scaled)
+
+plot(impact)
 
 
 
-#inserting chapter 5 p146 notation and quap formula
 
-#Ki ∼ Normal(µi, σ)
-#µi = α + βNNi + βMMi
-#α ∼ Normal(0, 0.2)
-#βn ∼ Normal(0, 0.5)
-#βm ∼ Normal(0, 0.5)
-#σ ∼ Exponential(1)
-
-m5.7 <- quap(
-  alist(
-    K ~ dnorm( mu , sigma ) ,
-    mu <- a + bN*N + bM*M ,
-    a ~ dnorm( 0 , 0.2 ) ,
-    bN ~ dnorm( 0 , 0.5 ) ,
-    bM ~ dnorm( 0 , 0.5 ) ,
-    sigma ~ dexp( 1 )
-  ) , data=dcc )
-precis(m5.7)
-
-# Work on scaling to 1 - 7 
-# Making everything have the same impact 
-# What the heck this graph is and make it look regression and how to use it for prediction 
 
 
