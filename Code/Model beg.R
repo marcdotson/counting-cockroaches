7877e49b24e2c11add6d178418dfdfffe21b36da
Marc Dotson <marc_dotson@byu.edu>
2019-07-26 16:37:46 -0600

    Cleaned up some of the simulation code

--- Code/Model beg.R
+++ Code/Model beg.R
@@ -1,56 +1,29 @@
 # Simulating Data ---------------------------------------------------------
-#simulating data for the model 
-
+# Load packages.
 library(rethinking)
 library(devtools)
 library(usethis)
 
-
-#length of tweet
-length_of_tweet <- sample(10:280, size = 1000 , replace = TRUE )
-
-
-#Have they tweeted about this in the past 
-past <- sample(0:1 , size = 1000 , replace = TRUE )
-
-
-#How many times has this re-tweeted 
-re_tweet <- runif(1000, min=0, max=5)
-
- 
-#How many followers they have
-followers <- sample(0:1000, size=1000, replace=TRUE)
-
-#scaling data 
-length_of_tweet_scaled <- scale(length_of_tweet)
-
-past_scaled <- scale(past)
-
-re_tweet_scaled <- scale(re_tweet)
-
-followers_scaled <- scale(followers)
-
-View(d_scaled)
+# Simulate explanatory variables.
+length_of_tweet <- sample(10:280, size = 1000 , replace = TRUE ) # Length of tweet.
+past <- sample(0:1 , size = 1000 , replace = TRUE )              # Have they tweeted about this in the past.
+re_tweet <- runif(1000, min=0, max=5)                            # How many times has this re-tweeted.
+# followers <- sample(0:1000, size=1000, replace=TRUE)             # How many followers they have.
+followers <- sample(0:1000, size=1000, replace=TRUE)             # How many followers they have.
 
 # Specify parameter values.
 b0 <- 1
-bl <- 1
-bp <- 1
-br <- 1
-bf <- 10
+bl <- 5
+bp <- 10
+br <- -3
+bf <- 2
 error <- rnorm(1000, 0, 1) 
 
 # Generate y.
 mydata <- b0 + bl * length_of_tweet + bp * past + br * re_tweet + bf * followers + error
 
-#Generate y_scaled 
-mydata_scaled <- b0 + bl *length_of_tweet_scaled + bp * past_scaled + br * re_tweet_scaled + bf * followers_scaled + error
-
 # Combine into data frame.
 d <- data.frame(mydata, length_of_tweet, past, re_tweet, followers)
-
-#new scaled data frame
-d_scaled <- data.frame(length_of_tweet_scaled,past_scaled,re_tweet_scaled, followers_scaled)
 
 # Estimation --------------------------------------------------------------
 # Run quap.
@@ -67,20 +40,6 @@ twitter <- quap(
   data = d
 )
 
-#quap scaled 
-twitter_scaled <- quap(
-  alist( 
-    mydata_scaled ~ dnorm(mu, sigma) , 
-    mu <- bl*length_of_tweet_scaled + bp*past_scaled + br*re_tweet_scaled + bf*followers_scaled , 
-    bl ~ dnorm(0, .25), 
-    bp ~ dnorm(0, .25), 
-    br ~ dnorm(0, .25), 
-    bf ~ dnorm(0, .25), 
-    sigma ~ dexp(1) 
-  ), 
-  data = d_scaled
-)
-
 # Evaluating model fit.
 precis(twitter_scaled)
 plot(twitter_scaled)
@@ -95,7 +54,6 @@ plot(impact)
 #Adam 
 
 #McKenna
-
 
 WAIC(twitter_scaled)
 
